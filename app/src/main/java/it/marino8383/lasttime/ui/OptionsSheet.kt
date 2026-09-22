db168e44a563afab324810f28be4e0768e9d0b84
marino8383 <fabrizio.mari@mapsgroup.it>
2026-09-22 09:56:38 +0200

    Import mancante in OptionsSheet

--- app/src/main/java/it/marino8383/lasttime/ui/OptionsSheet.kt
+++ app/src/main/java/it/marino8383/lasttime/ui/OptionsSheet.kt
@@ -18,6 +18,7 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
